519b56c37fa84159caf4283891ff92d983315cde
unknown <justinhoang0312@gmail.com>
2024-10-31 11:39:26 -0400

    Update post content to return the reaction counts for user-specific posts

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -117,7 +117,7 @@ async function getUserPosts(req, res) {
 
   try {
     const result = await db.query(
-      "SELECT posts.id, posts.content, posts.title, posts.created_at, posts.updated_at, posts.author_username, users.profile_pic_file, users.profile_pic_url FROM posts INNER JOIN users ON posts.author_username = users.username WHERE author_username = $1",
+      "SELECT posts.id, posts.content, posts.title, posts.created_at, posts.updated_at, posts.author_username, posts.likecount, posts.helpfulcount, posts.brilliantcount, users.profile_pic_file, users.profile_pic_url FROM posts INNER JOIN users ON posts.author_username = users.username WHERE author_username = $1",
       [username]
     );
     if (result.rows.length === 0) {
